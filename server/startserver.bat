e7ea1e18bf54bdac83c397859457ea3c741ec052
Kane Hart <kanechart@gmail.com>
2020-01-21 00:49:25 -0500

    Startserver.bat: Added restart system

--- server/startserver.bat
+++ server/startserver.bat
@@ -1,1 +1,9 @@
-java -Xms16G -Xmx16G -jar forge-1.7.10-10.13.4.1614-1.7.10-universal.jar nogui
+:begin
+
+java -XX:+UseG1GC -Xms16G -Xmx16G -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -jar forge-1.7.10-10.13.4.1614-1.7.10-universal.jar nogui
+
+timeout 10
+
+echo resuming server...
+
+goto begin
