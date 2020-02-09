06f7e7aeaf7af875e2a817d396571773aa99ae28
Kane Hart <kanechart@gmail.com>
2020-02-09 12:21:38 -0500

    Added alphasite.lua & -fix.lua
    This is a fun script to open the hanger doors via green bundle cable redstone coming in as well play the alarm via black bundle cable out.

---
+++ opencomputers/alphasite-fix.lua
@@ -0,0 +1,29 @@
+local debug = require("component").debug
+
+debug.runCommand("say Resetting Alpha Site")
+
+debug.runCommand("//pos1 x,y,z")
+debug.runCommand("//pos2 x,y,z")
+debug.runCommand("//set 42")
+os.sleep( 1 )
+
+debug.runCommand("//pos1 x,y,z")
+debug.runCommand("//pos2 x,y,z")
+debug.runCommand("//set 42")
+os.sleep( 1 )
+
+debug.runCommand("//pos1 x,y,z")
+debug.runCommand("//pos2 x,y,z")
+debug.runCommand("//set 42")
+os.sleep( 1 )
+
+debug.runCommand("//pos1 x,y,z")
+debug.runCommand("//pos2 x,y,z")
+debug.runCommand("//set 42")
+os.sleep( 1 )
+
+debug.runCommand("//pos1 x,y,z")
+debug.runCommand("//pos2 x,y,z")
+debug.runCommand("//set 42")
+os.sleep( 1 )
+
---
+++ opencomputers/alphasite.lua
@@ -0,0 +1,53 @@
+local computer = require("computer")
+local component = require("component")
+local sides = require("sides")
+local colors = require("colors")
+local rs = component.redstone
+local debug = require("component").debug
+  print(rs.getBundledInput(sides.right, colors.green))
+  while rs.getBundledInput(sides.right, colors.green) == 0 do
+os.sleep(1);
+  print("Green Redstone OFF")
+
+
+  end
+
+rs.setBundledOutput(sides.right, { [colors.black] = 15 } )
+os.sleep(1);
+debug.runCommand("say Alpha Base Hangers about to Open")
+
+os.sleep(5);
+  print("Green Redstone ON")
+
+os.sleep(1);
+
+
+debug.runCommand("//pos1 x,y,z")
+debug.runCommand("//pos2 x,y,z")
+debug.runCommand("//set 0")
+os.sleep( 1 )
+
+debug.runCommand("//pos1 x,y,z")
+debug.runCommand("//pos2 x,y,z")
+debug.runCommand("//set 0")
+os.sleep( 1 )
+
+debug.runCommand("//pos1 x,y,z")
+debug.runCommand("//pos2 x,y,z")
+debug.runCommand("//set 0")
+os.sleep( 1 )
+
+debug.runCommand("//pos1 x,y,z")
+debug.runCommand("//pos2 x,y,z")
+debug.runCommand("//set 0")
+os.sleep( 1 )
+
+debug.runCommand("//pos1 x,y,z")
+debug.runCommand("//pos2 x,y,z")
+debug.runCommand("//set 0")
+os.sleep( 1 )
+
+os.sleep( 5 )
+rs.setBundledOutput(sides.right, { [colors.black] = 0 } )
+print("Hangers Open Terminating Computer")
+computer.shutdown(false)
