7eff7cde29e5365e81706d567c939890e64f9ea1
Kane Hart <kanechart@gmail.com>
2020-02-09 14:23:59 -0500

    OC: teleport.lua Added logging & time stamping

--- opencomputers/teleport.lua
+++ opencomputers/teleport.lua
@@ -1,14 +1,14 @@
---[[
-Thanks to Rdjrjqouqcu for helping with this script.
+--[[Thanks to Rdjrjqouqcu for helping with this script.
 
 This script uses the motion sensor and debug card to grab players
 and teleport them via spread and then set their Spawnpoint to that location.
-]]--
+
+requires https://github.com/ShadowKatStudios/OC-Minitel/tree/master/realtime]]--
 
 
 local event = require("event")
 local component = require("component")
-
+local fs = require("filesystem")
 component.motion_sensor.setSensitivity(0)
 
 while true do
@@ -16,4 +16,10 @@ while true do
 
   component.debug.runCommand("spreadplayers 0 0 4000 12000 false " .. name)
   component.debug.runCommand("spawnpoint " .. name)
+
+date = os.date(nil,realtime.time())
+file = fs.open("/home/teleport-logs.txt","a")
+file:write(name .." " .. date .. "\n")
+file:close()
+
 end
