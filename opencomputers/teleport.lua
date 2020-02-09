a7e993d0518c2daa02d4ed88a262bf679bf3c0bb
Kane Hart <kanechart@gmail.com>
2020-02-09 12:14:35 -0500

    Added teleport.lua
    Used to Spread players and set their spawnpoint

---
+++ opencomputers/teleport.lua
@@ -0,0 +1,19 @@
+--[[
+Thanks to Rdjrjqouqcu for helping with this script.
+
+This script uses the motion sensor and debug card to grab players
+and teleport them via spread and then set their Spawnpoint to that location.
+]]--
+
+
+local event = require("event")
+local component = require("component")
+
+component.motion_sensor.setSensitivity(0)
+
+while true do
+  eventname, address, x, y, z, name = event.pull("motion")
+
+  component.debug.runCommand("spreadplayers 0 0 4000 12000 false " .. name)
+  component.debug.runCommand("spawnpoint " .. name)
+end
