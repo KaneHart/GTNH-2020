e62f29a5ec1b0ac9f3fef708323ee1b4e2322807
Kane Hart <kanechart@gmail.com>
2020-02-09 20:06:30 -0500

    OC: teleport.lua - added cords logging

--- opencomputers/teleport.lua
+++ opencomputers/teleport.lua
@@ -9,17 +9,24 @@ requires https://github.com/ShadowKatStudios/OC-Minitel/tree/master/realtime]]--
 local event = require("event")
 local component = require("component")
 local fs = require("filesystem")
+local debug = require("component").debug
 component.motion_sensor.setSensitivity(0)
+
 
 while true do
   eventname, address, x, y, z, name = event.pull("motion")
 
-  component.debug.runCommand("spreadplayers 0 0 4000 12000 false " .. name)
-  component.debug.runCommand("spawnpoint " .. name)
+  debug.runCommand("spreadplayers 0 0 4000 12000 false " .. name)
+  debug.runCommand("spawnpoint " .. name)
+  x,y,z = debug.getPlayer(name).getPosition()
+
 
 date = os.date(nil,realtime.time())
 file = fs.open("/home/teleportlog.txt","a")
-file:write(name .." " .. date .. "\n")
+
+file:write("\n")
+file:write(string.format(name .." " .. date .. " " .. "%.1f,%.1f,%.1f",x,y,z .. "\n"))
+
 file:close()
 
 end
