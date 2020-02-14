638f83384088c0586bd8ded5e6c48c1dedee8ed3
Kane Hart <kanechart@gmail.com>
2020-02-14 14:34:43 -0500

    StartServer.sh: 8GB to 12GB

--- server/startserver.sh
+++ server/startserver.sh
@@ -2,7 +2,7 @@
  
 while true
 do
-   java -XX:+UseG1GC -Xms8G -Xmx8G -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -jar forge-1.7.10-10.13.4.1614-1.7.10-universal.jar nogui
+   java -XX:+UseG1GC -Xms12G -Xmx12G -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -jar forge-1.7.10-10.13.4.1614-1.7.10-universal.jar nogui
     echo "If you want to completely stop the server process now, press Ctrl+C before the time is up!"
     echo "Rebooting in:"
     for i in 12 11 10 9 8 7 6 5 4 3 2 1
